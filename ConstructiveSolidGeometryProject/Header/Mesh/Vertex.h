2a771cbb9bedd5ceebf2c100d51022f7bf24557d
Scheres,Ruan R.H.W <ruanscheres12@gmail.com>
2023-12-15 10:39:43 +0100

    [ADDED] Setup constructors for the vertex class

--- ConstructiveSolidGeometryProject/Header/Mesh/Vertex.h
+++ ConstructiveSolidGeometryProject/Header/Mesh/Vertex.h
@@ -2,5 +2,11 @@
 #include "raymath.h"
 
 struct Vertex {
+public:
 	Vector3 m_position;
+
+public:
+	Vertex();
+	Vertex(float x, float y, float z);
+	Vertex(Vector3 position);
 };
---
+++ ConstructiveSolidGeometryProject/Header/Primitives/Vertex.cpp
@@ -0,0 +1,15 @@
+#include "Mesh/Vertex.h"
+
+Vertex::Vertex() :
+	m_position(Vector3())
+{}
+
+Vertex::Vertex(float x, float y, float z) {
+	m_position.x = x;
+	m_position.y = y;
+	m_position.z = z;
+}
+
+Vertex::Vertex(Vector3 position) :
+	m_position(position)
+{}
